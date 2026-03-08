5ab3f76e3e448bead98dbc0b85c7d7ae37218fc6
G Lokesh Kumar <lokeshkumarg4164@gmail.com>
2026-03-08 17:51:06 +0530

    added hackathon achievement

--- src/data/achievements.ts
+++ src/data/achievements.ts
@@ -3,18 +3,18 @@ export const achievements = [
     id: "ach1",
     title: "Rank Holder 2024-25",
     description: "Ranked 8st in the department(CSBS) for the 2024-2025 academic year.",
-    year: "2023",
+    year: "2025",
   },
   {
     id: "ach2",
-    title: "Smart India Hackathon 2025 – Cleared internal evaluation",
-    description: "Participated in Smart India Hackathon 2025 and cleared internal evaluation.",
-    year: "2025",
+    title: "Netrik National Hackathon 2026 – 4th place",
+    description: "Secured 4th place in Netrik National Hackathon 2026.",
+    year: "2026",
   },
   {
     id: "ach3",
     title: "Open Source Contributor",
-    description: "Contributed to open source projects and received recognition for my  contributions during Hacktoberfest and GirlScript Summer of Code.",
+    description: "Received Super contributor badge for Hacktoberfest 2025.",
     year: "2025",
   },
 
